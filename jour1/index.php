f65eca24d2f929f7f58a5b63c54fcacbab257ab0
Laura THAY <laura.thay@ipag.fr>
2020-05-11 12:11:13 +0200

    1er cours php
    11 /05/ 20

--- jour1/index.php
+++ jour1/index.php
@@ -1,20 +1,49 @@
 <h1> Thay thay </h1>
 
 
+
 <?php
-
-
 
 echo "a moi";;
 
 echo "<br>";
 
-echo "Jai fait caca "; // essayer de trouver lantislash
+echo "J\'ai fait caca "; // essayer de trouver lantislash
 
 echo "<h2> Les variables </h2>";
 
+?>
 
 
+
+
+<?php
+$chaine1 = "Ma chaîne";
+$chaine2 = "Ma seconde chaîne";
+
+echo "<br>";
+echo gettype($chaine1);
+echo"<br>";
+echo $chaine1 . " " . $chaine2;  // pour concaténer on met un .
+
+echo "<hr>";
+
+$n = 99;
+
+
+echo $n;
+echo "<br>";
+echo gettype($n);
+
+echo "<br>";
+$d = $n / 4;
+echo $d;
+echo"<br>";
+echo gettype($d);
+
+echo "<hr>";
+
+echo 4 % 2 ;
 
 
 
