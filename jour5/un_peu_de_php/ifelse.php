9a32bf01f5fd772a66b417e5dda0bbe41681a547
Laura THAY <laura.thay@ipag.fr>
2020-05-15 09:49:22 +0200

    jour 5 et correction exo veille
    php gettype strlen empty trim isset et === if else

--- jour5/un_peu_de_php/ifelse.php
+++ jour5/un_peu_de_php/ifelse.php
@@ -1,19 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Jour 3</title>
+    <title>Jour 5</title>
     <style>
         body {
-            text-align: center;
+            text-align: left;
+            padding-left: 20px;
         }
 
-        .btn{
+        nav a{
             border:1px #ccc solid;
             padding: 3px 10px;
             text-decoration: none;
             background-color: #eee;
             color-adjust: #000;
             margin-right: 10px;
+        }
+
+        h1 {
+            background: #ccc;
+            padding: 10px;
+        }
+
+        h2 {
+            border-bottom:1px #ccc dotted;
+            border-top:1px #ccc dotted;
+            margin-top: 40px;
+            padding: 20px 0 5px;
         }
 
     </style>
@@ -26,7 +39,7 @@
 <?php
 
 $a = 10;
-$b = 4;
+$b = 6;
 $c = 5;
 $d = 10;
 
@@ -40,7 +53,7 @@ if ($a > $b) {
     echo 'NON : c\'est $b qui est supérieur (ou égale) à $a <br>';
 }
 
-//exit;
+
 
 if ($b >= $c) {
     echo 'OUI : $b est supérieur ou égale à $c <br>';
@@ -48,39 +61,50 @@ if ($b >= $c) {
     echo 'NON : $b est strictement inférieur à $c <br> ';
 }
 
-//exit; //pour arreter tout ce qui a après
-
 // exercice :
 //  - vérifier que a est inférieur à b
-if ($a < $b){
-  echo "oui";
-} else{
-  echo "non";
-}
-//  - vérifier que a est inférieur ou égale à b
-if ($a <= $b){
-  echo "oui";
-} else{
-  echo "non";
-}
-//  - vérifier que a est égale à d
-if ($a == $d){
-  echo "oui";
-} else{
-  echo "non";
-}
-//  - vérifier que a est différent de c
-if ($a != $d){
-  echo "oui";
-} else{
-  echo "non";
+
+if($a < $b) {
+  echo "Oui \$a < \$b";
+} else {
+  echo 'NON $b <= $a';
 }
 
+echo "<br>";
+
+//  - vérifier que a est inférieur ou égale à b
+if($a <= $b) {
+  echo "Oui \$a <= \$b";
+} else {
+  echo 'NON $b < $a';
+}
+
+
+
+//  - vérifier que a est égale à d
+echo "<br>";
+
+if($a == $d) {
+  echo "OUI";
+} else {
+  echo "NON";
+}
+
+echo "<br>";
+
+//  - vérifier que a est différent de c
+
+if($a != $c) {
+  echo '$a != $c';
+} else {
+  echo '$a == $c';
+}
 
 
 
 
 echo "<h3>Comparaison par valeur VS comparaison par valeur et type</h3>";
+
 $integer = 10;
 $string = "10";
 
@@ -96,7 +120,9 @@ if($integer === $string) {
     echo 'NON : $integer est différent de $string (en valeur OU en type) <br>';
 }
 
-echo "<h2>Combiner plusieurs comparaison</h2>";
+
+
+echo "<h2>Combiner plusieurs comparaisons</h2>";
 
 echo "<h3>AND (et)</h3>";
 
@@ -105,6 +131,8 @@ if ($a > $b && $a < $c ) {
 } else {
     echo 'NON : au moins une de conditions est fausse<br>';
 }
+
+
 
 echo "<h3>OR  (ou)</h3>";
 
@@ -115,27 +143,19 @@ if ($a > $b || $a < $c ) {
 }
 
 
-
-
-
 // if else elseif
 
 $a = 40;
 
 if($a == 5) {
     echo 'REP 1 : $a est egale à 5 <br>';
-} elseif( $a > 9) {
-    echo 'REP 2 : $a est supérieur à 9 <br>'; // quand une condition est validé on sort du if
-
-} elseif ($a > 10){
-    echo 'REP 2bis: $a est supérieur à 10 <br>'; // cette ligne ne sert a rien donc
-} else {
+  } elseif( $a > 9) {
+      echo 'REP 2 : $a est supérieur à 9 <br>';
+  } elseif( $a > 10) {
+      echo 'REP 2bis : $a est supérieur à 10 <br>';
+  } else {
     echo 'REP 3 : $a n\'est pas supérieur à 9 et n\'est pas égale à 5 <br>';
 }
-
-
-
-
 
 echo "<h2>Forme contractée (opérateur ternaire)</h2>";
 $a = 5;
@@ -143,54 +163,58 @@ $a = 5;
 // forme contractée
 echo ($a >= 5) ? '$a est suppérieur ou égale à 5 <br>' : '$a strictement inférieur à 5 <br>';
 
-// forme longue
-if ($a>= 5)
+if ($a >= 5) {
   echo '$a est suppérieur ou égale à 5 <br>';
-} else if ($a<= 5){
+} else {
   echo '$a strictement inférieur à 5 <br>';
 }
 
-
-
-
-
-
+$a = 5;
 $new = ($a == 5) ? 'OUI' : 'NON';
-echo $new;
 
-// autre facon de l'écrire
-$($a == 5){
+if($a == 5) {
   $new = "OUI";
 } else {
   $new = "NON";
 }
 
-echo $new;
-
-
-
-
-
-
-
-
-
 
 echo "<h2>Fonctions pour savoir si une variable est définie</h2>";
+
+
+
+if(empty($yoyo) == FALSE) {
+  echo $yoyo;
+}
+
+if( !empty($yoyo) ) {
+  echo $yoyo;
+}
+
+if(isset($yoyo)) {
+  echo $yoyo;
+} else {
+  echo "pas yoyo";
+}
+
+echo "<hr>";
+
 
 // empty( $var ) : retrourne FALSE si la $var existe, qu'elle n'est pas vide et que la valeur n'est pas 0, "0", FALSE, NULL
 // isset($var) : retourne TRUE si $var est définit même si elle est vide
 
-$var = 0;
+$var = 1;
 // Evalué à vrai car $var est vide
-if (empty($var)) { // si ma variable est vide ca ecrit le echo
+
+if (empty($var)) {
     echo 'A - $var vaut soit 0, vide, ou pas définie du tout <br/>';
 }
 
 // Evalué à vrai car $var est défini
-if (isset($var)) { // est ce que ma variable var est defini si oui echo
+if (isset($var)) {
     echo 'B - $var est définie même si elle est vide <br/>';
 }
+
 
 if (isset($var_2)) {
     echo 'C - Je peux traiter $var_2 car je sais qu\'elle est définit <br/>';
@@ -198,6 +222,17 @@ if (isset($var_2)) {
     echo 'D - Je ne peux pas traiter  $var_2 car cette variable n\'a pas été définit <br/>';
 }
 
+echo "<hr>";
+
+
+
+if( !isset($prof)) {
+  $prof = "Nicolas";
+}
+
+echo $prof. " est super ";
+
+echo "<hr>";
 
 // verifier qu'une variable n'est pas vide
 $var_3 = "Je ne suis pas vide !";
@@ -208,10 +243,12 @@ if( !empty($var_3)) {
     // c'est à dire que TRUE devient FALSE
     // et FALSE devient TRUE
     // ici on pourrait dire "SI PAS empty()"
-    echo '$var_3 n\'est pas vide et ne vaut pas 0 <br>';
+    echo 'REP 1 : $var_3 n\'est pas vide et ne vaut pas 0 <br>';
 } else {
-    echo '$var_3 est pas vide ou vaut 0 <br>';
+    echo 'REP 2 : $var_3 est pas vide ou vaut 0 <br>';
 }
+
+
 
 // façon de tester : //
 if(  TRUE) { // "si pas vrai"
@@ -220,33 +257,26 @@ if(  TRUE) { // "si pas vrai"
     echo "FALSE <br>";
 }
 
-echo "<hr>";
-var_dump(!empty($var_3));  // var_dump nous aide pour avoir des infos plus précise sur les variables
 
 echo "<hr>";
-var_dump(empty ($var_3));
+var_dump(!empty($var_3));
+// Fonction qui permet d'avoir des informations utiles sur la variable passée en paramétre.
+// Utile pendant la période de développement.
 
-// Tester en affectant des valeurs différente à la variable & en commentant la ligne
+echo "<hr>";
+var_dump(empty($var_3));
+
 
 echo "<h4>Exercice</h4>";
 // EXO 1 : vérifier que ma variable $var_45 est egale à 0.
 // SI OUI, écrire : la variable est égale 0
 // SI NON : ne rien faire
-echo "<hr>";
 
-$var_45 = 0;
-
-// j'aurai pas pu l'écrire $var_45 == 0 && isset($var_45) pcq l'ordre est très important
-if (isset($var_45) && $var_45 == 0) { // vérifier si la variable est défini on utilise isset et vide avec empty donc on aurait écrit !empty
-  echo "la variable est égale à 0";
-} else{
-  echo "je passe par cet accolade";
-}
 
 
 echo "<h2>Conditions switch</h2>";
 
-$fruit = 'kiwi';
+$fruit = 'poire';
 
 switch ($fruit) {
     case 'kiwi' :
@@ -256,6 +286,7 @@ switch ($fruit) {
         break;  // obligatoire pour sortir du switch
     case 'banane' :
         echo 'J\'aime les bananes <br>';
+        echo 'Oui !! <br>';
         break;
     case 'pomme' :
         echo 'J\'aime les pommes <br>';
@@ -266,47 +297,37 @@ switch ($fruit) {
 }
 
 
-$fruit = "amande";
+$fruit = "poire";
 
-if ($fruit == "amande") {
-    elseif ($ftuit == 'kiwi') :
-    elseif ($fruit =='banane') :
+switch ($fruit) {
+    case 'kiwi' :
+    case 'banane' :
         echo 'J\'aime les fruits exotiques <br>';
-    elseif ($fruit == 'poire') {
-        echo'J\'aime les machins';
-    } elseif($fruit =='pomme') :
-    elseif($fruit == 'poire') :
+        break;  // obligatoire pour sortir du switch
+    case 'pomme' :
+    case 'poire' :
         echo 'J\'aime les fruits classiques <br>';
-    elseif ($fruit == 'amande') {
+        break;
+    case 'amande' :
         echo 'J\'aime les graines <br>';
-    else :  // cas par défaut : aucun des précédents cas n'a été validé.
+        break;
+    default :  // cas par défaut : aucun des précédents cas n'a été validé.
         echo 'J\'ai l\'impression que je n\'aime pas trop les fruits<br>';
+        break;
 }
 
-// correction
-
-$fruit = "amande";
-if ($fruit == 'kiwi' || $fruit == 'banane') {
-  echo 'J\'aime les fruits exotiques';
-}
-else if ($fruit == 'pomme' || $fruit == 'poire') {
-  echo 'J\'aime les fruits classiques';
-}
-else if ($fruit == 'amande') {
-  echo 'J\'aime les graines';
-}
-else {
-  echo 'J\'ai l\'impression que je n\'aime pas trop les fruits';
-}
-
-// EXO : réécrire le dernier switch avec des if / else pour avoir le même résultat
-
+#
+# EXERCICE : réécrire le dernier switch avec des if / else pour avoir le même résultat
+#
 
 ?>
-<hr>
-<a href="index.php" class="btn">Les bases </a>
-<a href="ifelse.php" class="btn">Conditions & Comparaisons</a>
-<a href="fonctions.php" class="btn">Les fonctions</a>
 
+
+
+
+<hr>
+<?php
+include("nav.php");
+?>
 </body>
 </html>
