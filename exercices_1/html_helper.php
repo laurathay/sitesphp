94c83e926c6807b8026a967af3a4201738345e2e
Laura THAY <laura.thay@ipag.fr>
2020-05-25 11:18:27 +0200

    correction html helper

--- exercices_1/html_helper.php
+++ exercices_1/html_helper.php
@@ -7,19 +7,22 @@
 // les paramètres url_lien et $texteest sont obligatoires
 // quand $new_window est à 1, notre lien ouvrira une nouvelle fenêtre
 //
+function html_a($texte, $url, $class = '', $new_window = FALSE){
+
+  if($new_window == TRUE){ // on pourrait simplement indiquer if($new_window) => par défaut TRUE
+    $new_window = "_blank";
+  } else {
+    $new_window = "";
+  }
+  $resultat = "<a href='$url' class='$class' target='$new_window'> $texte </a>" . PHP_EOL;
+  return $resultat;
+}
 // ex :
-// echo html_a("Les décodeuses", "https://www.lesdecodeuses.fr", "lien_rouge", TRUE);
+ echo html_a("Les décodeuses", "https://www.lesdecodeuses.fr", "lien_rouge", TRUE);
 // écrire
 // <a href='https://www.lesdecodeuses.fr' class='lien_rouge' target='_blank'>Les décodeuses</a>
 
-fonction html_a( $texte , $url,  $class, $new_window){
-  $texte = "<p> Les descodeuses </p>";
-  $url = "<a href='http://www.lesdecodeuses.fr'</a>";
-  $class = 'div class='lien_rouge'';
-  $new_window = 'target='_blank'';
 
-  echo html_a();
-};
 
 
 // 2 -
@@ -29,18 +32,18 @@ fonction html_a( $texte , $url,  $class, $new_window){
 //
 // ex :
 // echo html_tag("div", "Vivement lundi", "texte_rouge");
+//
 // écrit :
 // <div class="texte_rouge">Vivement lundi</div>
 
- fonction html_tag($nomDuTag, $contenu, $class){
-   $nomDuTag = 'div';
-   $contenu ="Vivement ludni" ;
-   $class = 'texte_rouge' ;
+function html_tag($nomDuTag, $contenu, $class = ''){
+  return "<$nomDuTag class='$class'>$contenu<$nomDuTag>" . PHP_EOL;
+}
 
-   echo html_tag();
- };
+echo html_tag("div", "Vivement lundi", "texte_rouge");
+echo html_tag("div", "Vivement ludni");
 
-
+echo "<hr>" . PHP_EOL;
 
 
 //  3 -
@@ -56,12 +59,25 @@ fonction html_a( $texte , $url,  $class, $new_window){
 //    <li>pomme</li>
 // <ul>
 
-fonction html_ul($tableau){
-    $tableau = '<ul>';
-    while ($donnees = ["kiwi"], ["banane"], ["pomme"]){
-      $tableau .= '<li> "kiwi, banane, pomme "</li>';
-  }
-  $tableau .='</ul>';
-}
+           function html_ul($tableau){
+             $chaine_puces = ""; // on créer a vide
+             $chaine_puces .= "<ul>"; // on concaténe avec la balise ul
 
-echo ($tableau);
+             foreach ($tableau as $valeur){ // tableau que je passe en parametre et chaque case de ce tableau je continue a créer une chaine de caractere
+                $chaine_puces .="<li> $valeur </li>" . PHP_EOL;  // je concacténe les li
+             }
+             $chaine_puces .= "</ul>"; //la on ferme la balise ul
+             return $chaine_puces;
+           }
+
+           echo html_ul(["kiwi", "banane", "pomme"]);
+
+
+          //     $tableau = '<ul>';
+          //     while ($donnees = ["kiwi"], ["banane"], ["pomme"]){
+          //       $tableau .= '<li> "kiwi, banane, pomme "</li>';
+          //   }
+          //   $tableau .='</ul>';
+          // }
+          //
+          // echo ($tableau);
