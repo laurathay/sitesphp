1bc4b4a8cff9581639c674afb54191bc7dc85f44
Laura THAY <laura.thay@ipag.fr>
2020-06-05 16:43:10 +0200

    jusqu'a la question 2
    tout marche a moitié ...

--- exercice_newsletter/formulaire.php
+++ exercice_newsletter/formulaire.php
@@ -2,6 +2,27 @@
 
     include "config.php";
 
+    if(!empty($_SESSION["peut_se_connecter"])){
+        header("location:formulaire.php");
+        exit;
+    }
+
+// 1 / Vérifier que la personne a bien rempli le champ email. 
+// Si le champ email est correcte (c'est à dire qu'il est rempli et qu'il s'agit d'un email), on l'enregistre.
+// Dans le cas contraire, on retourne sur le formulaire en mettant un message d'erreur.
+
+// 2 / dans la page "lister_newsletter.php", lister dans un tableau l'ensemble des inscriptions. (une colonne nom, prénom et email)
+
+// 3 / dans le tableau précédent, ajouter une colonne "modifier" et sur chaque ligne un bouton "modifier".
+// Quand on clique sur ce bouton, on arrive sur un formulaire qui va nous permettre de modifier les données de l'enregistrement choisi.
+// (par exemple, si on clique sur le bouton "modifier" en face de la ligne nicolas hennette, on arrive sur un nouveau formulaire pré-remplit qui va me permettre de modifier mon nom, mon prénom et mon email)
+
+// * je pense que vous allez devoir ajouter dans ce formulaire de modification un champ "caché" qui aura la valeur de l'identifiant de l'inscription que j'ai choisi de modifier.
+//  (<input type='hidden' value='identifiant a récupérer'>)
+
+// 4 / Dans le tableau qui liste l'ensemble des inscriptions, ajouter un second bouton pour chaque ligne "Effacer".
+// Quand on clique sur ce bouton, on efface la ligne choisie.
+
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -26,12 +47,45 @@
 
 
 <form action="reponse.php" method="post">
+
+<?php if(!empty($_GET['err']) && $_GET['err'] == 'champ'){
+                            echo "<div clas=\"error \">";
+                            echo "merci de vérifier";
+                            echo "</div>";
+                          }
+                          
+                          function addClassErreurChamp($champVide){
+                            if(!empty($_SESSION['problemechamp']) && $_SESSION ['problemechamp']== $champVide){
+                              echo"erreurchamp";
+                            }
+                          }
+
+                            //erreur dans le champ
+                            if(!empty($_GET['err']) && $_GET['err'] == 'identification'){
+                                echo "<div clas=\"error \">";
+                                echo "merci de vérifier vos id et mdp";
+                                echo "</div>";
+                                unset($_SESSION['err']);
+                              }
+
+
+ ?>
+
     <input type="text" placeholder="Nom" name="fnom">
     <input type="text" placeholder="Prénom" name="fprenom">
     <input type="text" placeholder="Email" name="femail">
     <br><br>
     <input type="submit">
 </form>
+
+<?php
+
+//INSERT INTO `inscription` (`id_inscription`, `nom`, `prenom`, `email`) VALUES (NULL, 'Thay', 'Thay', 'thay@gmail.com');
+
+$ajout_inscription ="INSERT INTO `inscription` (`id_inscription`, `nom`, `prenom`, `email`) VALUES('. $_POST[nom].', '.$_POST[prenom].', '.$_POST[email]' )";
+    echo $ajout_inscription;
+
+?>
 
 
 </body>
--- exercice_newsletter/lister_newsletter.php
+++ exercice_newsletter/lister_newsletter.php
@@ -1,11 +1,18 @@
 <?php
 
+$inscriptions = array[
+    "nom" => "thay <button> Modifier </button> <button> Effacer </button>",
+    "prenom" => "thay <button> Modifier </button> <button> Effacer </button>",
+    "email" => "thay@gmail.com <button> Modifier </button> <button> Effacer </button>",
+];
+var_dump($inscriptions);
+
 /*
  *
  *
   * Ici, je veux lister l'ensemble des inscriptions dans un tableau.
  * En face de chaque nom, prenom, email, je veux un bouton "Modifier" qui va aller vers un formulaire qui va me permettre de modifier l'enregistrement
  * et un autre bouton "Effacer" qui va me permettre d'effacer les données de la ligne en question.
- * 
+ *
  *
  */
