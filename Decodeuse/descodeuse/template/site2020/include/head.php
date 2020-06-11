dc9753fd70ecf94b78effe488013fe5d4b9a120b
Laura THAY <laura.thay@ipag.fr>
2020-06-11 12:28:55 +0200

    css ne marche pas
    probleme quelque part

---
+++ Decodeuse/descodeuse/template/site2020/include/head.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- emoticones et fonts -->
+    <script src="https://kit.fontawesome.com/682d71be2a.js" crossorigin="anonymous"></script>
+
+
+    <!------ bootstrap articles ----->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- boootstrap gallery --->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <title> Thay Thay </title>
+    <link rel="stylesheet" href="<?php echo URL_TEMPLATE ?>css/style.css">
+
+  </head>
+  <body>
+    <header>
+
+    <!-- barre de navigation -->
+
+
+        <a href="index.html"> About </a>
+        <a href="project.html"> Projects</a>
+        <a href="contact.html"> Contact </a>
+
+    </header>
